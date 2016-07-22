e4bf412e4b9e5c131a2510fce2fd7a59d2a4c73e
John Talbot <jat0021@tigermail.auburn.edu>
2016-07-21 22:56:49 -0500

    Remove LED Debug

--- waterLevelMeasurement.c
+++ waterLevelMeasurement.c
@@ -46,12 +46,6 @@ ISR(INT0_vect){
 }
 
 ISR(TIMER1_OVF_vect){
-	LED_PORT |= (1<<LED_PIN);
-	_delay_ms(100);
-	LED_PORT &= ~(1<<LED_PIN);
-
-
-
 	// Generate a 12us pulse to trigger the HR-SR04
 	TRIGGER_PORT &= ~(1<<TRIGGER_PIN);
 	_delay_us(15);
