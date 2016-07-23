0b3693044690f8fbecb6951ff6f8e60923fc243a
John Talbot <jat0021@tigermail.auburn.edu>
2016-07-22 22:58:02 -0500

    Change c code to trigger sensor read over UART, include python script to control UART

--- readUpdateLevel.py
+++ readUpdateLevel.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
 
+#-------------------------------------------------------------------
+# HEADER
+#-------------------------------------------------------------------
+# John Talbot
+# July 22, 2016
+# This program will trigger an atmega over UART to read range. It
+# will then populate a google sheet with the range data
+
+#-----------------------------
+# IMPORT PACKAGES
+#-----------------------------
+import os
+import time
+import serial
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+#------------------------------
+# INITIALIZE GOOGLE SHEET DATA
+#------------------------------
 worksheetName = "rangeTest"
-
 
 scope = ['https://spreadsheets.google.com/feeds']
 
@@ -13,4 +29,33 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name('/home/jat0021/De
 gc = gspread.authorize(credentials)
 
 worksheet = gc.open(worksheetName).sheet1
+
+#--------------------------------
+# INITIALIZE SERIAL PORT
+#--------------------------------
+SERIAL_PORT = "/dev/ttyAMA0"
+BAUD_RATE = 9600
+
+sp = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout = 5)
+sp.flushInput()
+sp.flushOutput()
+
+#---------------------------------
+# MAIN PROGRAM LOOP
+#---------------------------------
+while(1)
+	os.system('clear')
+	sp.write(b'\x01')
+	time.sleep(0.1)
+	dataRead = sp.read(4)
+	if dataRead == b'\xFFFFFFFF':
+		print("Sensor Timeout Error")
+	elif dataRead == b'\xDDDDDDDD':
+		print("UART Data Error")
+	else:
+		print("Distance = %04d cm" % dataRead, end="")
+		worksheet.update_cell(2,3, dataRead)
+
+	time.sleep(1)
+
 
--- waterLevelMeasurement.c
+++ waterLevelMeasurement.c
@@ -5,28 +5,33 @@ This program measures distance using an HC-SR04 ultrasonic sensor and then trans
 */
 
 #ifndef F_CPU
-#define F_CPU 8000000UL
+#define F_CPU 				8000000UL
 #endif
 
-#define TRIGGER_PIN		PD4
-#define TRIGGER_PORT 	PORTD
-#define TRIGGER_DDR		DDRD
+#define TRIGGER_PIN			PD4
+#define TRIGGER_PORT 		PORTD
+#define TRIGGER_DDR			DDRD
 
-#define ECHO_PIN		PB0
-#define ECHO_PORT		PORTB
-#define ECHO_DDR		DDRB
+#define ECHO_PIN			PB0
+#define ECHO_PORT			PORTB
+#define ECHO_DDR			DDRB
 
-#define LED_PIN			PC0
-#define LED_PORT		PORTC
-#define LED_DDR			DDRC
+#define LED_PIN				PC0
+#define LED_PORT			PORTC
+#define LED_DDR				DDRC
 
-#define DIST_THRESH 	5
+#define DIST_THRESH 		5
+
+#define UART_TRIG_BYTE 		0x01
+#define SENSOR_TIMEOUT		0xFFFFFFFF
+#define UART_BAD_RECEIVE 	0xDDDDDDDD
 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include "USART.h"
 
+volatile uint8_t byteReceived;
 volatile uint16_t beginCount, endCount;
 volatile uint32_t elapsedCounts;
 volatile uint32_t distance;
@@ -37,9 +42,9 @@ static inline void initTimer1(void){
 	TIMSK1 |= (1<<ICIE1) | (1<<TOIE1) | (1<<ICNC1);
 }
 
-static void sendDistance(void){
-	printWord(distance>>16);
-	printWord(distance);
+static void sendData(uint32_t dataToSend){
+	printWord(dataToSend>>16);
+	printWord(dataToSend);
 	transmitByte(0x0D);
 }
 
@@ -62,22 +67,32 @@ ISR(TIMER1_CAPT_vect){
 		endCount = ICR1;
 		distance = (endCount - beginCount) / 58;
 		goodReturn = 1;
-		sendDistance();
+		sendData(distance);
 		TCCR1B |= (1<<ICES1);
 	}
 }
 
 ISR(TIMER1_OVF_vect){
 	if(!goodReturn){
-		distance = 0xFFFFFFFF;
-		sendDistance();
+		sendData(SENSOR_TIMEOUT);
 		TCNT1 = 0;
 		TCCR1B |= (1<<ICES1);
 	}
 }
 
+ISR(USART_RX_vect){
+	byteReceived = receiveByte();
+	if(byteReceived == UART_TRIG_BYTE){
+		TCNT1 = 0;
+		generatePulse();
+	}
+	else{
+		sendData(UART_BAD_RECEIVE);
+	}
+}
+
 int main(){
-	//LED_DDR |= (1<<LED_PIN);			//Set data direction out for LED pin
+	LED_DDR |= (1<<LED_PIN);			//Set data direction out for LED pin
 	TRIGGER_DDR |= (1<<TRIGGER_PIN);	//Set data direction out for TRIGGER pin
 
 	initUSART();
@@ -85,9 +100,8 @@ int main(){
 	sei();
 
 	while(1){
-		_delay_ms(1000);
-		TCNT1 = 0;
-		generatePulse();
+		_delay_ms(750);
+		LED_PORT ^= (1<<LED_PIN);
 	}
 
 	return(0);
