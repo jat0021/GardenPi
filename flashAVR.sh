635f00347cd80e4b8d1d427a2c61bb69915e5b7b
John Talbot <jat0021@tigermail.auburn.edu>
2016-07-26 02:21:12 -0500

    Add comments to flashAVR.sh to improve readability

--- flashAVR.sh
+++ flashAVR.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 # Test avrdude programming Atmega328p using UART 
 
+# Pull AVR reset pin low
 gpio -g mode 25 out
 gpio -g write 25 0
+
+# Compile AVR C code before continuing
 make
-sleep 0.05
+
+# Pull AVR reset pin high
 gpio -g write 25 1
 sleep 0.05
+
+# flash AVR chip with program
 make flash
 sleep 0.25
+
+# Change reset pin into HiZ input to safely keep AVR in active mode
 gpio -g mode 25 in
 gpio -g mode 25 up
