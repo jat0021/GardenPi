44382b581c4130473d44c867f2f361ddd56be9ff
John Talbot <jat0021@tigermail.auburn.edu>
2016-07-27 22:34:31 +0000

    Add LED debug to Rx ISR

--- GardenPi.c
+++ GardenPi.c
@@ -81,7 +81,7 @@ static void readWaterTankLvl(uint8_t sensorCode){
 	}	
 
 	// Enable timer1 overflow interrupt
-	TIMSK1 |= (1<<TOIE1);
+	//TIMSK1 |= (1<<TOIE1);
 
 	// Loop until water level interrupt flag is set
 	while(!wtrLvlISRDone){}
@@ -166,6 +166,11 @@ ISR(TIMER1_OVF_vect){
 
 // UART receive interrupt 
 ISR(USART_RX_vect){
+	uint8_t i;
+	for(i=0; i<6; i++){
+		LED_DEBUG2_PORT ^= LED_DEBUG2_PIN;
+	}
+
 	// Set byte received flag
 	byteReceived = 1;
 
