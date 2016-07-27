0f3e0e9dcb88426aadfc0104f4165f0c2b211c50
John Talbot <jat0021@tigermail.auburn.edu>
2016-07-26 23:52:57 -0500

    Add mid level python UART functions

--- UART.py
+++ UART.py
@@ -19,4 +19,31 @@ BAUD_RATE = 9600
 # MID LEVEL UART FUNCTIONS
 #------------------------------------------------------------------
 # Reply to request with ready byte
-sp.write()
+def transmitReady():
+	sp.write(RASPI_READY)
+	sp.write(END_MSG)
+
+def transmitRequest():
+	sp.write(RASPI_REQ_AVR)
+	sp.write(END_MSG)
+
+def transmitConfirm():
+	sp.write(RASPI_REC_MSG_CONFIRM)
+	sp.write(END_MSG)
+
+def transmitInitialize():
+	sp.write(RASPI_INIT_TO_AVR)
+	sp.write(END_MSG)
+
+def transmitPackage( dataPackage ):
+	i = 0
+	length = len(dataPackage)
+	while(i < length):
+		sp.write(dataPackage[i])
+		i += 1;
+	sp.write(END_MSG)
+
+#------------------------------------------------------------------
+# HIGH LEVEL UART FUNCTIONS
+#------------------------------------------------------------------
+
